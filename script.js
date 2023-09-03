b764514a2ba43c719fe23b407db8fa55ff716871
skmajumder <skmajumder.cse@gmail.com>
2023-09-04 01:57:52 +0600

    Editor Setup & Compilation

---
+++ script.js
@@ -0,0 +1,11 @@
+// const userName = "Shuvo";
+// console.log(userName);
+// const arrFun = () => {
+//     console.log('Arrow Function');
+// }
+var user = {
+  firstName: "Angela",
+  lastName: "Davis",
+  role: "Professor",
+};
+console.log(user.name);
---
+++ script.ts
@@ -0,0 +1,7 @@
+// const userName = "Shuvo";
+// console.log(userName);
+
+// const arrFun = () => {
+//     console.log('Arrow Function');
+
+// }
