fda127a04134257906b835105544fa50d16b775f
skmajumder <skmajumder.cse@gmail.com>
2023-09-04 13:53:34 +0600

    Type Basics

--- output/script.js
+++ output/script.js
@@ -1,6 +1,34 @@
 "use strict";
-var userName = "Shuvo";
-console.log(userName);
-var arrFun = function () {
-    console.log('Arrow Function');
+// const userName: string = "Shuvo";
+// console.log(userName);
+// const arrFun = () => {
+//     console.log('Arrow Function');
+// }
+// let playerName: string = "Messi";
+// console.log(playerName);
+// let str;
+// str = "Bangldesh";
+// str = 53;
+// function multiply(a: number, b: number) {
+//     return a * b;
+// }
+// console.log(multiply(5, 4));
+// let names = ['Jonas', 'Margaret', 'Ariana']
+// let names: string[] = ['Jonas', 'Margaret', 'Ariana']
+// let numbers: number[] = [1, 2, 3, 4, 5]
+// numbers.push(6, 7, 8)
+// console.log(numbers);
+// tuple
+// let ourTuple: [number, boolean, string] = [5, true, 'Bangladesh'];
+// Named Tuples
+// const graph: [x: number, y: number] = [55.2, 41.3];
+var sportsMan = {
+    name: 'Sakib',
+    age: 35,
+    isCaptain: true,
 };
+// let sportsMan2 = {
+//     name: 'Sakib',
+//     age: 35,
+//     isCaptain: true,
+// }
--- src/script.ts
+++ src/script.ts
@@ -1,7 +1,48 @@
-const userName = "Shuvo";
-console.log(userName);
+// const userName: string = "Shuvo";
+// console.log(userName);
 
-const arrFun = () => {
-    console.log('Arrow Function');
 
+// const arrFun = () => {
+//     console.log('Arrow Function');
+
+// }
+
+// let playerName: string = "Messi";
+// console.log(playerName);
+
+// let str;
+
+// str = "Bangldesh";
+// str = 53;
+
+// function multiply(a: number, b: number) {
+//     return a * b;
+// }
+
+// console.log(multiply(5, 4));
+
+// let names = ['Jonas', 'Margaret', 'Ariana']
+// let names: string[] = ['Jonas', 'Margaret', 'Ariana']
+
+// let numbers: number[] = [1, 2, 3, 4, 5]
+// numbers.push(6, 7, 8)
+
+// console.log(numbers);
+
+// tuple
+// let ourTuple: [number, boolean, string] = [5, true, 'Bangladesh'];
+
+// Named Tuples
+// const graph: [x: number, y: number] = [55.2, 41.3];
+
+let sportsMan: { name: string, age: number, isCaptain: boolean } = {
+    name: 'Sakib',
+    age: 35,
+    isCaptain: true,
 }
+
+// let sportsMan2 = {
+//     name: 'Sakib',
+//     age: 35,
+//     isCaptain: true,
+// }
